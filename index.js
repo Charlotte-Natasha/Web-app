19204c19e64ff96e7d1c28a8e77b085b5eba6390
Charlotte Natasha <natashaonyango97@gmail.com>
2022-03-05 23:31:57 +0300

    added dob and dd

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ button.addEventListener('click', (e)=>{
     e.preventDefault();
     let dateOfBirth=document.getElementById('date').value; 
    let gender = document.forms['details'].gender.value
-   console.log(gender);
+   //checking date validity
+   console.log(dateOfBirth);
+   if (parseInt(dateOfBirth.slice(5, 7))<= 12 && parseInt(dateOfBirth.slice(8, 10))<= 31){
+    let dOb= new Date(dateOfBirth)
+    let dd= dOb.getDay()
+    console.log(dd);   
+
+   } 
+   else {
+       alert('You have entered invalid date/month.')
+   }
+
+
+
 } );
+
+
+
