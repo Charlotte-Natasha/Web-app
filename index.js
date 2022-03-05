c9751cf9afa31eff0a04d2ad2e66389ef1946c89
Charlotte Natasha <natashaonyango97@gmail.com>
2022-03-06 02:04:19 +0300

    added the last akan name

--- index.js
+++ index.js
@@ -13,28 +13,34 @@ button.addEventListener('click', (e)=>{
     //Assigning the Akan names.
     let akanName = '';
     if(dd === 0){
-        (gender = 'Female') ? akanName = 'Akosua' : akanName = 'Kwasi'
+        (gender == 'Female') ? akanName = 'Akosua' : akanName = 'Kwasi'
     }   
     else if (dd === 1){
-        (gender = 'Female') ? akanName = 'Adwao' : akanName = 'Kwadwo'
+        (gender == 'Female') ? akanName = 'Adwao' : akanName = 'Kwadwo'
     }
     else if (dd === 2){
-        (gender = 'Female') ? akanName = 'Abenaa' : akanName = 'Kwabena'
+        (gender == 'Female') ? akanName = 'Abenaa' : akanName = 'Kwabena'
     }
     else if (dd === 3){
-        (gender = 'Female') ? akanName = 'Akua' : akanName = 'Kwaku'
+        (gender == 'Female') ? akanName = 'Akua' : akanName = 'Kwaku'
     }
     else if (dd === 4){
-        (gender = 'Female') ? akanName = 'Yaa' : akanName = 'Yaw'
+        (gender == 'Female') ? akanName = 'Yaa' : akanName = 'Yaw'
     }
     else if (dd === 5){
-        (gender = 'Female') ? akanName = 'Afua' : akanName = 'Kofi'
+        (gender == 'Female') ? akanName = 'Afua' : akanName = 'Kofi'
     }
     else if (dd === 6){
-        (gender = 'Female') ? akanName = 'Ama' : akanName = 'Kwame'
+        (gender == 'Female') ? akanName = 'Ama' : akanName = 'Kwame'
     }
+else {
 
-
+}
+console.log (akanName);
+let detailLog=document.getElementById('select')
+detailLog.innerHTML=`
+<p> Your Akan name is : ${akanName}</p>
+`
    } 
    else {
        alert('You have entered invalid date/month.')
