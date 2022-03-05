8e7cc9c3d8537a7f0cbd9689a2ac45d3a19a75f8
Charlotte Natasha <natashaonyango97@gmail.com>
2022-03-05 23:45:19 +0300

    added akua names

--- index.js
+++ index.js
@@ -3,12 +3,37 @@ button.addEventListener('click', (e)=>{
     e.preventDefault();
     let dateOfBirth=document.getElementById('date').value; 
    let gender = document.forms['details'].gender.value
+   console.log(gender);
    //checking date validity
    console.log(dateOfBirth);
    if (parseInt(dateOfBirth.slice(5, 7))<= 12 && parseInt(dateOfBirth.slice(8, 10))<= 31){
     let dOb= new Date(dateOfBirth)
     let dd= dOb.getDay()
-    console.log(dd);   
+    console.log(dd);
+    //Assigning the Akan names.
+    let akanName = '';
+    if(dd === 0){
+        (gender = 'Female') ? akanName = 'Akosua' : akanName = 'Kwasi'
+    }   
+    else if (dd === 1){
+        (gender = 'Female') ? akanName = 'Adwao' : akanName = 'Kwadwo'
+    }
+    else if (dd === 2){
+        (gender = 'Female') ? akanName = 'Abenaa' : akanName = 'Kwabena'
+    }
+    else if (dd === 3){
+        (gender = 'Female') ? akanName = 'Akua' : akanName = 'Kwaku'
+    }
+    else if (dd === 4){
+        (gender = 'Female') ? akanName = 'Yaa' : akanName = 'Yaw'
+    }
+    else if (dd === 5){
+        (gender = 'Female') ? akanName = 'Afua' : akanName = 'Kofi'
+    }
+    else if (dd === 6){
+        (gender = 'Female') ? akanName = 'Ama' : akanName = 'Kwame'
+    }
+
 
    } 
    else {
